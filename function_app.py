5826dc170b7d40ae3de8c7ab23802005e7825137
Allahviranloo <sahand.allahviranloo@gmail.com>
2025-11-22 14:09:50 -0400

    Updated dependencies.

--- function_app.py
+++ function_app.py
@@ -49,3 +49,26 @@ def RetrieveData(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+
+@app.blob_trigger(arg_name="myblob", path="input-files/{name}",
+                               connection="os.environ["AzureWebJobsStorage"]") 
+def BlobProcessor(myblob: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob"
+                f"Name: {myblob.name}"
+                f"Blob Size: {myblob.length} bytes")
+
+
+# This example uses SDK types to directly access the underlying BlobClient object provided by the Blob storage trigger.
+# To use, uncomment the section below and add azurefunctions-extensions-bindings-blob to your requirements.txt file
+# Ref: aka.ms/functions-sdk-blob-python
+#
+# import azurefunctions.extensions.bindings.blob as blob
+# @app.blob_trigger(arg_name="client", path="input-files/{name}",
+#                   connection="os.environ["AzureWebJobsStorage"]")
+# def BlobProcessor(client: blob.BlobClient):
+#     logging.info(
+#         f"Python blob trigger function processed blob \n"
+#         f"Properties: {client.get_blob_properties()}\n"
+#         f"Blob content head: {client.download_blob().read(size=1)}"
+#     )
